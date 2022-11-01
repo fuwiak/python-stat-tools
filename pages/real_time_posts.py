21247cd9a440c11e19171af65062e19292098102
Леонид Каретников <poczta130@gmail.com>
2022-11-01 21:14:41 +0000

    add: template.html

---
+++ pages/real_time_posts.py
@@ -0,0 +1,6 @@
+import streamlit as st
+from streamlit.components.v1 import iframe
+import pandas as pd
+
+st.set_page_config(layout="centered", page_icon="🎓", page_title="Real Time Posts")
+st.title("Real Time Posts")
